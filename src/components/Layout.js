9d17c02c431341bf421321e9ac3877ff672224bc
suriyagokul <124340052+suriyagokul@users.noreply.github.com>
2023-02-20 10:43:15 +0530

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -35,9 +35,9 @@ function Layout({ children }) {
             className="justify-content-end p-2"
             id="basic-navbar-nav">
             <Nav className="ml-auto" >
-                  <Link to="/services">Services</Link>
-                  <Link to="/about">About</Link>
-                  <Link to="/testimonials">Testimonials</Link>
+                  <Link className="text-6xl text-white hover:text-gray-200 mr-2" to="/services">Services</Link>
+                  <Link className="text-6xl text-white hover:text-gray-200 mr-2" to="/about">About</Link>
+                  <Link className="text-6xl text-white hover:text-gray-200" className="text-6xl text-white hover:text-gray-200" to="/testimonials">Testimonials</Link>
             </Nav>
             <Link to="/" onClick={userLogout} style={{ textDecoration: 'none'}} className="text-red-500">Logout</Link>
           </Navbar.Collapse>
