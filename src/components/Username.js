3c2d6b34a3c6c2d22fbb1c7cd4855367a3d4d993
suriyagokul <124340052+suriyagokul@users.noreply.github.com>
2023-02-21 15:08:57 +0530

    Update Username.js

--- src/components/Username.js
+++ src/components/Username.js
@@ -1,4 +1,4 @@
-import React from "react";
+ import React, { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import avatar from "../assets/profile.png";
 import { Toaster } from "react-hot-toast";
@@ -9,9 +9,10 @@ import { useAuthStore } from "../store/store";
 import styles from "../styles/Username.module.css";
 
 export default function Username() {
-  const navigate = useNavigate();
-  const setUsername = useAuthStore((state) => state.setUsername);
 
+  const navigate = useNavigate();
+  const setUsername = useAuthStore(state=>state.setUsername);
+ 
   const formik = useFormik({
     initialValues: {
       username: "",
@@ -21,55 +22,58 @@ export default function Username() {
     validateOnChange: false,
     onSubmit: async (values) => {
       setUsername(values.username);
-      navigate("/password");
+      navigate('/password');
     },
   });
 
   return (
     <div className="font-poppins bg-[#dcdce6] bg-venkamala">
-      <div className="container mx-auto">
-        <Toaster position="top-center" reverseOrder={false}></Toaster>
-        <div className="flex justify-center items-center h-screen sm:h-auto">
-          <div className={styles.glass}>
-            <div className="title flex flex-col items-center">
-              <h2 className="text-sm font-bold sm:text-base">D2D HealthCare</h2>
-              <h4 className="text-3xl font-bold sm:text-4xl">hello amigo!</h4>
-              <span className="py-2 text-base text-center text-gray-500 sm:text-lg">
-                Explore More by connecting with us.
-              </span>
+    <div className="container mx-auto" >
+      <Toaster position="top-center" reverseOrder={false}></Toaster>
+      <div className="flex justify-center items-center h-screen">
+        <div className={styles.glass}>
+          <div className="title flex flex-col items-center">
+            <h2 className="text-sm font-bold">D2D HealthCare</h2>
+            <h4 className="text-5xl font-bold">hello amigo!</h4>
+            <span className="py-2 text-l  text-center text-gray-500 ">
+              Explore More by connecting with us.
+            </span>
+          </div>
+
+          <form className="py-1" onSubmit={formik.handleSubmit}>
+            <div className="profile flex justify-center">
+              <img src={avatar} alt="avatar" className={styles.profile} />
             </div>
 
-            <form className="py-1" onSubmit={formik.handleSubmit}>
-              <div className="profile flex justify-center">
-                <img src={avatar} alt="avatar" className={styles.profile} style={{ width: "60px", height: "60px" }} />
+            <div className="textbox flex flex-col items-center">
+              <input
+                type="text"
+                className={styles.textbox}
+                placeholder="Username"
+                {...formik.getFieldProps("username")}
+              />
+              <div className="py-3  flex justify-center">
+                <button type="submit" className={styles.btn}>
+                  Let's Go
+                </button>
               </div>
+            </div>
 
-              <div className="textbox flex flex-col items-center">
-                <input
-                  type="text"
-                  className={styles.textbox}
-                  placeholder="Username"
-                  {...formik.getFieldProps("username")}
-                />
-                <div className="py-3 flex justify-center">
-                  <button type="submit" className={styles.btn}>
-                    Let's Go
-                  </button>
-                </div>
-              </div>
-
-              <div className="py-2 text-center">
-                <span className="text-gray-400 text-base sm:text-lg">
-                  Not a Member?{" "}
-                  <Link to="/register" className="text-red-500">
-                    Register Now
-                  </Link>
-                </span>
-              </div>
-            </form>
-          </div>
+            <div className="py-2 text-center">
+              <span className="text-gray-400">
+                Not a Member?{" "}
+                <Link to="/register" className="text-red-500">
+                  Register Now
+                </Link>
+              </span>
+            </div>
+            {/* Using react router dom Link instead of html anchor tag.. Because if we use a tag it will refresh and moves onto another page.
+                  In SPA we don't use like this By using Link page dynamically changes without reloading.. 
+                */}
+          </form>
         </div>
       </div>
+    </div>
     </div>
   );
 }
